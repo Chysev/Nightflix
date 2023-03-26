36967c6a4ab61a46d9e672572fdd59e5a2e28416
Chysev <Johnlayda92@gmail.com>
2023-03-26 14:47:20 +0400

    Initial Commit

---
+++ components/LandingPage/Layer1.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mantine/core"
+
+function Layer1() {
+    return (
+        <div className="videocontainer">
+            <Box className="videos">
+                Vidoes
+
+            </Box>
+        </div>
+    )
+}
+
+export default Layer1
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
+import { useState } from 'react';
 import type { AppProps } from 'next/app'
+import { useColorScheme } from '@mantine/hooks';
+import { SessionProvider } from "next-auth/react"
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+
+import Layout from '@/components/Layout';
+
+import '@/global/global.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  // Themes
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <SessionProvider session={pageProps.session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
