fb7fef8e54884acb9963b6777a1a05aacb955876
Chysev <Johnlayda92@gmail.com>
2023-03-30 21:12:49 +0400

    Finished

--- components/Navs/AuthSideNav/IsAuth.tsx
+++ components/Navs/AuthSideNav/IsAuth.tsx
@@ -9,7 +9,7 @@ import { HiOutlineHome } from "@react-icons/all-files/hi/HiOutlineHome";
 import { MdSubscriptions } from "@react-icons/all-files/md/MdSubscriptions"
 import { AiOutlineLogout } from "@react-icons/all-files/ai/AiOutlineLogout"
 import { CgArrowsExchange } from '@react-icons/all-files/cg/CgArrowsExchange'
-import { ActionIcon, Box, Divider, Navbar, useMantineColorScheme, Text, Avatar, Popover, Button, Paper } from "@mantine/core"
+import { ActionIcon, Box, Divider, Navbar, useMantineColorScheme, Text, Avatar, Popover, Button, Paper, ScrollArea } from "@mantine/core"
 
 function IsAuth() {
 
@@ -21,106 +21,108 @@ function IsAuth() {
 
     return (
         <Navbar className="sidenav" maw={300}>
-            <Navbar.Section>
-                <Link className="middle" href="/">
-                    <Text color={dark ? '#339af0' : '#339af0'} fz={16}>
-                        <h1 className="font1 nightflix">NIGHTFLIX</h1>
-                    </Text>
-                </Link>
+            <ScrollArea>
+                <Navbar.Section>
+                    <Link className="middle" href="/">
+                        <Text color={dark ? '#339af0' : '#339af0'} fz={16}>
+                            <h1 className="font1 nightflix">NIGHTFLIX</h1>
+                        </Text>
+                    </Link>
 
-                <Divider my="sm" labelPosition="center"
-                    label={<ActionIcon
-                        variant="outline"
-                        color={dark ? 'yellow' : 'blue'}
-                        onClick={() => toggleColorScheme()}
-                        title="Toggle color scheme"
-                    >
-                        {dark ? <BiSun size="1.1rem" /> : <BiMoon size="1.1rem" />}
-                    </ActionIcon>} />
-
-            </Navbar.Section>
-
-            <Navbar.Section className="sideicons" grow maw={200}>
-                <Link href="/">
-                    <Box className="li" maw={102}>
-                        <HiOutlineHome color={dark ? '#339af0' : '#339af0'} size={28} />
-                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Home</p></Text>
-                    </Box>
-                </Link>
-
-                <Link href="/">
-                    <Box className="li" maw={110}>
-                        <RiMovieFill color={dark ? '#339af0' : '#339af0'} size={28} />
-                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Course</p></Text>
-                    </Box>
-                </Link>
-
-                <Link href="/">
-                    <Box className="li" maw={106}>
-                        <FaTrailer color={dark ? '#339af0' : '#339af0'} size={28} />
-                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Starter</p></Text>
-                    </Box>
-                </Link>
-
-                <Link href="/">
-                    <Box className="li" maw={162}>
-                        <MdSubscriptions color={dark ? '#339af0' : '#339af0'} size={28} />
-                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Subscription</p></Text>
-                    </Box>
-                </Link>
-
-                <Link href="/">
-                    <Box className="li" maw={130}>
-                        <AiFillHeart color={dark ? '#339af0' : '#339af0'} size={28} />
-                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Favourite</p></Text>
-                    </Box>
-                </Link>
-            </Navbar.Section>
-
-
-            <Navbar.Section>
-
-                <Popover width={300} radius="md" position="top" withArrow>
-                    <Popover.Target>
-                        <Divider my="sm" labelPosition="center"
-                            label={<Avatar className="avatar" src={session?.user?.image || undefined} size={45} radius={120} mx="auto" />} />
-                    </Popover.Target>
-                    <Popover.Dropdown>
-                        <Paper
-                            radius="md"
-                            withBorder
-                            p="lg"
-                            sx={(theme) => ({
-                                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-                            })}
+                    <Divider my="sm" labelPosition="center"
+                        label={<ActionIcon
+                            variant="outline"
+                            color={dark ? 'yellow' : 'blue'}
+                            onClick={() => toggleColorScheme()}
+                            title="Toggle color scheme"
                         >
-                            <Avatar src={session?.user?.image || undefined} size={120} radius={120} mx="auto" />
-                            <Text className="font2" ta="center" fz="lg" weight={500} mt="md">
-                                {session?.user?.name}
-                            </Text>
-                            <Text className="font2" ta="center" c="dimmed" fz="sm">
-                                {session?.user?.email} • NIGHTFLIX
-                            </Text>
+                            {dark ? <BiSun size="1.1rem" /> : <BiMoon size="1.1rem" />}
+                        </ActionIcon>} />
 
-                        </Paper>
-                    </Popover.Dropdown>
-                </Popover>
+                </Navbar.Section>
+
+                <Navbar.Section className="sideicons" grow maw={200}>
+                    <Link href="/">
+                        <Box className="li" maw={102}>
+                            <HiOutlineHome color={dark ? '#339af0' : '#339af0'} size={28} />
+                            <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Home</p></Text>
+                        </Box>
+                    </Link>
+
+                    <Link href="/">
+                        <Box className="li" maw={110}>
+                            <RiMovieFill color={dark ? '#339af0' : '#339af0'} size={28} />
+                            <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Course</p></Text>
+                        </Box>
+                    </Link>
+
+                    <Link href="/">
+                        <Box className="li" maw={106}>
+                            <FaTrailer color={dark ? '#339af0' : '#339af0'} size={28} />
+                            <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Starter</p></Text>
+                        </Box>
+                    </Link>
+
+                    <Link href="/">
+                        <Box className="li" maw={162}>
+                            <MdSubscriptions color={dark ? '#339af0' : '#339af0'} size={28} />
+                            <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Subscription</p></Text>
+                        </Box>
+                    </Link>
+
+                    <Link href="/">
+                        <Box className="li" maw={130}>
+                            <AiFillHeart color={dark ? '#339af0' : '#339af0'} size={28} />
+                            <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Favourite</p></Text>
+                        </Box>
+                    </Link>
+                </Navbar.Section>
 
 
-                <Link href="/">
-                    <Box onClick={() => signIn()} className="li" maw={210}>
-                        <CgArrowsExchange color={dark ? '#339af0' : '#339af0'} size={36} />
-                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Change Account</p></Text>
+                <Navbar.Section>
+
+                    <Popover width={300} radius="md" position="top" withArrow>
+                        <Popover.Target>
+                            <Divider my="sm" labelPosition="center"
+                                label={<Avatar className="avatar" src={session?.user?.image || undefined} size={45} radius={120} mx="auto" />} />
+                        </Popover.Target>
+                        <Popover.Dropdown>
+                            <Paper
+                                radius="md"
+                                withBorder
+                                p="lg"
+                                sx={(theme) => ({
+                                    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+                                })}
+                            >
+                                <Avatar src={session?.user?.image || undefined} size={120} radius={120} mx="auto" />
+                                <Text className="font2" ta="center" fz="lg" weight={500} mt="md">
+                                    {session?.user?.name}
+                                </Text>
+                                <Text className="font2" ta="center" c="dimmed" fz="sm">
+                                    {session?.user?.email} • NIGHTFLIX
+                                </Text>
+
+                            </Paper>
+                        </Popover.Dropdown>
+                    </Popover>
+
+
+                    <Link href="/">
+                        <Box onClick={() => signIn()} className="li" maw={210}>
+                            <CgArrowsExchange color={dark ? '#339af0' : '#339af0'} size={36} />
+                            <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Change Account</p></Text>
+                        </Box>
+                    </Link>
+
+                    <Box onClick={() => signOut()} className="li" maw={135}>
+                        <AiOutlineLogout color={dark ? '#339af0' : '#339af0'} size={30} />
+                        <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Sign Out</p></Text>
                     </Box>
-                </Link>
 
-                <Box onClick={() => signOut()} className="li" maw={135}>
-                    <AiOutlineLogout color={dark ? '#339af0' : '#339af0'} size={30} />
-                    <Text color={dark ? '#c1c2c5' : '#495057'} fz={22}><p className="font2">Sign Out</p></Text>
-                </Box>
-
-            </Navbar.Section>
-        </Navbar>
+                </Navbar.Section>
+            </ScrollArea >
+        </Navbar >
     )
 }
 
